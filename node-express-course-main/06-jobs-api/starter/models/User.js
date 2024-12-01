a7a724bf0fd4e31615ade76b5751f0fc874f87f5
YamanNTG <claudiuoprea21@gmail.com>
2024-12-01 22:41:21 +0000

    token instance method in schema

--- node-express-course-main/06-jobs-api/starter/models/User.js
+++ node-express-course-main/06-jobs-api/starter/models/User.js
@@ -1,5 +1,6 @@
-const mongoose = require("/mongoose");
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,8 +21,18 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
-    maxlength: 12,
   },
 });
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jtwSecret", {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
