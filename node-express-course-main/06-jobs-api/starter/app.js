5a1011a4ce19dd6bcb29e57c49692f7eeda8d848
YamanNTG <claudiuoprea21@gmail.com>
2024-12-06 23:41:26 +0000

    Jobs API finished- starting file upload

--- node-express-course-main/06-jobs-api/starter/app.js
+++ node-express-course-main/06-jobs-api/starter/app.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security packages
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xxs = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
 //connectDB
 const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
 //routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -12,13 +21,21 @@ const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xxs());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
